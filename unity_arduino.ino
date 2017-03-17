574e26edba6346abf8a7eff20cd0ac7404fe8440
LoicHmh <hmh0205@qq.com>
2017-03-17 19:27:40 +0800

    Add files via upload

---
+++ unity_arduino.ino
@@ -0,0 +1,13 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int incomingByte=0;
+  if (Serial.available()>0)
+  {
+    incomingByte = Serial.read();
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
+}
